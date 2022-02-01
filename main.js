1fe06fc1bc35355fa0560a92c7e014f6dd2aee4e
unknown <tomasz.czechlewski@gmail.com>
2022-02-02 00:05:28 +0100

    simply weather app finished. Thanks for OpenWeather API

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const cityName = document.querySelector('.cityName')
 const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temp')
 const humid = document.querySelector('.humidity')
+const pres = document.querySelector('.pressure')
+const wind = document.querySelector('.wind')
 
 const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
 const API_KEY = '&appid=5b61511832775aa861c9d464a8fef32f'
@@ -18,14 +20,26 @@ const getWeather = () => {
 		const weat = res.data.weather[0].description
 		const hum = res.data.main.humidity
 		const temp = res.data.main.temp
-        const icon = res.data.weather[0].icon
-        weatherImg.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
-        humid.textContent = hum + "%"
-        weather.textContent =  weat 
-        temperature.textContent = Math.floor(temp) + '°C'
-        cityName.textContent = res.data.name
-console.log(weatherImg);
+		const icon = res.data.weather[0].icon
+		const pressure = res.data.main.pressure
+		const windSpeed = res.data.wind.speed
+
+		pres.textContent = pressure + ' hPa'
+		wind.textContent = windSpeed + ' m/s'
+		weatherImg.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+		humid.textContent = hum + '%'
+		weather.textContent = weat
+		temperature.textContent = Math.floor(temp) + '°C'
+		cityName.textContent = res.data.name
 	})
 }
+
+const enterKey = e => {
+	if (e.key === 'Enter') {
+		getWeather()
+	}
+}
+
 getWeather()
 btn.addEventListener('click', getWeather)
+input.addEventListener('keyup', enterKey)
